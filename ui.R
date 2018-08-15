2fed80a0b5f55a7932a599ec1ef633c48c98a026
yyyaaan <yan@yan.fi>
2018-08-15 09:26:21 +0000

    v2.1

--- ui.R
+++ ui.R
@@ -46,6 +46,7 @@ material_page(
         class = "col s12 m4", 
         material_card(
           "Yan PAN",
+          "M.Sc. Statistics + M.Sc. Economics",tags$br(), tags$br(),
           tags$i(class = "tiny material-icons", "email"), tags$a(href = "mailto:yan@yan.fi", "yan@yan.fi  "),
           tags$i(class = "tiny material-icons", "phone_in_talk"),  tags$a(href = "tel:+358449199857", "+358 44 919 9857  "),
           tags$br(),
@@ -103,7 +104,6 @@ material_page(
                    uiOutput("Ct")),
            tags$li(class = "collection-item",uiOutput("C"))
         )
-
       )
     )
   ),
@@ -111,7 +111,7 @@ material_page(
 
   
 
-  # 4th ---------------------------------------------------------------------
+  # 3rd ---------------------------------------------------------------------
   
   material_side_nav_tab_content(
     side_nav_tab_id = "tab4",
@@ -125,24 +125,13 @@ material_page(
     )
   ),
   
-  # 3rd ---------------------------------------------------------------------
+  # 4th ---------------------------------------------------------------------
   
   material_side_nav_tab_content(
     side_nav_tab_id = "tab3",
     tags$div(class = "col s12 m12",
-             #material_card("", "This brief letter seeks to deliver my application for credit risk analyst in Ikano Bank by elaborating relevant details about my strengths and experiences."),
-             
-             #material_card("Analytical-Minded Solution Maker", "In my working and studying, the task is always to find the solution despite limitation of systems and tools. Strong background in Economics, Finance and Statistics provides me with a analytics-driven mind that searches for the possibly best solutions and even push ahead in a highly organised manner. The recent project in my current position features for the innovative, reliable and cost-efficient cumulative data analysis/business intelligence, where self-learning in multiple programming language API is utilised. Similar to my previous full-time work in Bank of Shanghai, except for accuracy, emphasis is laid to beyond-numeric analysis that focuses on the implication, and thereafter, deliver the solutions, improvements and advancements. In addition, learning capability with eagerness for new knowledge continuously extends my skills."),
-             
-             #material_card("Competence and Experiences in both Finance and Statistics", "Since bachelor's study, corporate finance(incl. management accounting, risk management) has greatly attracted my interests. 3-year experience in a major regional branch of a large bank provided me further practical knowledge in financial markets. During both full-time work experiences, I have been actively dealing with accounting and financing affairs in an self-organised systematic manner that seeks for accuracy and efficient.  Empowered by modern stochastic models and statistical tools, such financial knowledge can be materialised into practical skills in insightful financial statement analysis, risk and liquidity modelling etc."),
-             
-             #material_card("Data Science Enthusiast and Finance Insider", "The meaning of data science is much more than study and work; it is a joyful entertainment for me. Whenever I explore some interesting information, financial statements are also first consulted, as I believe such audited information is unbiased and most reliable among public information. During the past few year, I have succeeded in pursuing academic competence without compromising full-time work"),
-             
-             #material_card("Team Intelligence", "Artificial Intelligence and Business Intelligence may shape the modern world; however, I firmly believe that Team Intelligence that promotes team efforts and smooth communication is the key to the success. For credit risk analysis, it is critical to communicate in depth to ensure the accurate understanding of the backgrounds, needs and objectives in addition to active learning."),
-                          
       material_card(
         "Yan Pan",
-        #tags$p("Salary wish: 2550 EUR/month (acceptable level 2250 EUR/month)"),
         tags$br(),
         tags$i(class = "tiny material-icons", "email"), tags$a(href = "mailto:yan@yan.fi", "yan@yan.fi  "),
         tags$i(class = "tiny material-icons", "phone_in_talk"),  tags$a(href = "tel:+358449199857", "+358 44 919 9857  "),
